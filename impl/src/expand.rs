45d231f7080bd3a7c0ca5f4b0dd9eb05888b7fc0
loichyan <loichyan@foxmail.com>
2021-06-16 23:37:59 +0800

    feat(macro): support generic context

--- impl/src/expand.rs
+++ impl/src/expand.rs
@@ -1,9 +1,14 @@
-use crate::utils::{punctuated_parse, punctuated_tokens, tokens_with, Attributes, Braced, Parened};
+use std::str::FromStr;
+
+use crate::utils::{
+    custom_token, tokens_with, AngleBracket, Attributes, Brace, Paren, ParseWith, Punctuated,
+    Surround, SurroundEnum, WithSurround,
+};
 use proc_macro2::TokenStream;
 use quote::{quote, ToTokens};
 use syn::parse::{Parse, ParseStream, Result};
-use syn::{punctuated::Punctuated, Ident, Token, Type};
-use syn::{token, FieldsNamed, FieldsUnnamed, Visibility};
+use syn::parse_quote;
+use syn::{token, Ident, Token, Type, Visibility};
 
 mod kw {
     use syn::custom_keyword;
@@ -29,18 +34,18 @@ impl ToTokens for ThisCtx {
 }
 
 struct Enum {
-    attr: Attributes,
+    attrs: Attributes,
     vis: Visibility,
     enum_token: token::Enum,
     name: Ident,
-    brace_token: token::Brace,
+    brace_token: Brace,
     variants: Punctuated<Variant, Token![,]>,
 }
 
 impl Enum {
     fn get_enum_def(&self) -> TokenStream {
         let Self {
-            attr,
+            attrs,
             vis,
             enum_token,
             name,
@@ -49,20 +54,19 @@ impl Enum {
         } = self;
         let body = tokens_with(|tokens| {
             brace_token.surround(tokens, |tokens| {
-                punctuated_tokens::<Token![,], _, _>(
-                    tokens,
-                    variants.iter().map(Variant::get_variant_def),
-                );
+                variants
+                    .to_tokens_with(Variant::get_variant_def)
+                    .to_tokens(tokens)
             })
         });
-        quote!(#attr #vis #enum_token #name #body)
+        quote!(#attrs #vis #enum_token #name #body)
     }
 
     fn get_context_def(&self) -> TokenStream {
         tokens_with(|tokens| {
             self.variants
                 .iter()
-                .for_each(|variant| variant.get_context_def(&self.vis).to_tokens(tokens));
+                .for_each(|variant| variant.get_context_def(&self.vis).to_tokens(tokens))
         })
         .to_token_stream()
     }
@@ -71,7 +75,7 @@ impl Enum {
         tokens_with(|tokens| {
             self.variants
                 .iter()
-                .for_each(|variant| variant.get_impl_into_error(&self.name).to_tokens(tokens));
+                .for_each(|variant| variant.get_impl_into_error(&self.name).to_tokens(tokens))
         })
         .to_token_stream()
     }
@@ -82,7 +86,7 @@ impl Enum {
                 variant
                     .get_impl_from_ctx_for_enum(&self.name)
                     .to_tokens(tokens)
-            });
+            })
         })
         .to_token_stream()
     }
@@ -90,14 +94,13 @@ impl Enum {
 
 impl Parse for Enum {
     fn parse(input: ParseStream) -> Result<Self> {
-        let attr = input.parse()?;
+        let attrs = input.parse()?;
         let vis = input.parse()?;
         let enum_token = input.parse()?;
         let name = input.parse()?;
-        let (brace_token, variants) =
-            Braced::parse_with(input, |input| input.parse_terminated(Variant::parse))?;
+        let (brace_token, variants) = Brace::parse_with(input, Punctuated::parse)?;
         Ok(Self {
-            attr,
+            attrs,
             vis,
             enum_token,
             name,
@@ -117,23 +120,20 @@ impl ToTokens for Enum {
 }
 
 struct Variant {
-    attr: Attributes,
+    attrs: Attributes,
     name: Ident,
     body: VariantBody,
 }
 
 impl Variant {
     fn get_variant_def(&self) -> TokenStream {
-        let Self {
-            attr,
-            name: variant_name,
-            body,
-        } = self;
+        let Self { attrs, name, body } = self;
+        let generic = body.get_context_generic_with(StructBody::get_generic_ty_f);
         let struct_body = body.get_struct_body(
-            variant_name,
+            &quote!(#name #generic),
             &tokens_with(|tokens| body.get_src_ty().to_tokens(tokens)),
         );
-        quote!(#attr #variant_name #struct_body)
+        quote!(#attrs #name #struct_body)
     }
 
     fn get_context_def(&self, vis: &Visibility) -> TokenStream {
@@ -161,12 +161,20 @@ impl Variant {
         let src_ty = body.get_src_ty().unwrap_or_else(|| quote!(#NONE_ERROR));
         let src_name = quote!(source);
         let expr_struct_body = body.get_struct_body(
-            &tokens_with(|tokens| quote!(self).to_tokens(tokens)),
+            &tokens_with(|tokens| {
+                let from = quote!(self);
+                let convert_struct_body = body.get_context_struct_body_with(|field| {
+                    StructBody::get_convert_struct_body_f(&from, field)
+                });
+                quote!(#variant_name #convert_struct_body).to_tokens(tokens)
+            }),
             &tokens_with(|tokens| src_name.to_tokens(tokens)),
         );
+        let generic_bounded = body.get_context_generic_with(StructBody::get_generic_bounded_f);
+        let generic_name = body.get_context_generic_with(StructBody::get_generic_name_f);
         quote!(
             #[allow(unused)]
-            impl #INTO_ERROR for #variant_name {
+            impl #generic_bounded #INTO_ERROR for #variant_name #generic_name {
                 type Error = #enum_name;
                 type Source = #src_ty;
 
@@ -187,11 +195,21 @@ impl Variant {
             return None;
         }
         let ctx_name = quote!(context);
-        let expr_struct_body = body.get_struct_body(&ctx_name, &tokens_with(|_| ()));
+        let expr_struct_body = body.get_struct_body(
+            &tokens_with(|tokens| {
+                let convert_struct_body = body.get_context_struct_body_with(|field| {
+                    StructBody::get_convert_struct_body_f(&ctx_name, field)
+                });
+                quote!(#variant_name #convert_struct_body).to_tokens(tokens)
+            }),
+            &tokens_with(|_| ()),
+        );
+        let generic_bounded = body.get_context_generic_with(StructBody::get_generic_bounded_f);
+        let generic_name = body.get_context_generic_with(StructBody::get_generic_name_f);
         let gen = quote!(
             #[allow(unused)]
-            impl From<#variant_name> for #enum_name {
-                fn from(#ctx_name: #variant_name) -> Self {
+            impl #generic_bounded From<#variant_name #generic_name> for #enum_name {
+                fn from(#ctx_name: #variant_name #generic_name) -> Self {
                     Self::#variant_name #expr_struct_body
                 }
             }
@@ -202,25 +220,21 @@ impl Variant {
 
 impl Parse for Variant {
     fn parse(input: ParseStream) -> Result<Self> {
-        let attr = input.parse()?;
-        let variant_name = input.parse()?;
+        let attrs = input.parse()?;
+        let name = input.parse()?;
         let body = input.parse()?;
-        Ok(Self {
-            attr,
-            name: variant_name,
-            body,
-        })
+        Ok(Self { attrs, name, body })
     }
 }
 
 enum VariantBody {
     Struct {
-        brace_token: token::Brace,
+        brace_token: Brace,
         src: SourceField,
         ctx: ContextField,
     },
     Tuple {
-        paren_token: token::Paren,
+        paren_token: Paren,
         anon_struct: Option<AnonStruct>,
     },
     Unit,
@@ -234,6 +248,41 @@ impl VariantBody {
                 SourceField::None => None,
             },
             Self::Tuple { .. } | Self::Unit => None,
+        }
+    }
+
+    fn get_context_generic_with<F>(&self, f: F) -> Option<TokenStream>
+    where
+        F: FnMut(&GenericField) -> TokenStream,
+    {
+        match self {
+            Self::Struct { ctx, .. } => match ctx {
+                ContextField::Some { anon_struct, .. } => anon_struct.body.get_generic_with(f),
+                ContextField::None => None,
+            },
+            Self::Tuple { anon_struct, .. } => anon_struct
+                .as_ref()
+                .map(|anon_struct| anon_struct.body.get_generic_with(f))
+                .flatten(),
+            Self::Unit => None,
+        }
+    }
+
+    fn get_context_struct_body_with<F>(&self, f: F) -> Option<TokenStream>
+    where
+        F: FnMut(&GenericField) -> TokenStream,
+    {
+        match self {
+            Self::Struct { ctx, .. } => match ctx {
+                ContextField::Some { anon_struct, .. } => {
+                    Some(anon_struct.body.get_struct_body(f).to_token_stream())
+                }
+                ContextField::None => None,
+            },
+            Self::Tuple { anon_struct, .. } => anon_struct
+                .as_ref()
+                .map(|anon_struct| anon_struct.body.get_struct_body(f).to_token_stream()),
+            Self::Unit => None,
         }
     }
 
@@ -261,12 +310,13 @@ impl VariantBody {
                     ContextField::None => None,
                 };
                 Some(
-                    tokens_with(|tokens| {
+                    tokens_with(move |tokens| {
                         brace_token.surround(tokens, |tokens| {
-                            punctuated_tokens::<Token![,], _, _>(
-                                tokens,
-                                src_field.iter().chain(&ctx_field),
-                            )
+                            src_field
+                                .iter()
+                                .chain(&ctx_field)
+                                .collect::<Punctuated<_, Token![,]>>()
+                                .to_tokens(tokens)
                         })
                     })
                     .to_token_stream(),
@@ -277,7 +327,11 @@ impl VariantBody {
                 anon_struct,
             } => anon_struct.as_ref().map(|_| {
                 tokens_with(|tokens| {
-                    paren_token.surround(tokens, |tokens| quote!(#ctx_rhs).to_tokens(tokens))
+                    paren_token.surround(tokens, |tokens| {
+                        std::iter::once(quote!(#ctx_rhs))
+                            .collect::<Punctuated<_, Token![,]>>()
+                            .to_tokens(tokens)
+                    })
                 })
                 .to_token_stream()
             }),
@@ -291,8 +345,8 @@ impl Parse for VariantBody {
         if input.peek(token::Brace) {
             let mut src = SourceField::None;
             let mut ctx = ContextField::None;
-            let (brace_token, _) = Braced::parse_with(input, |input| {
-                punctuated_parse::<Token![,], _>(input, |input| {
+            let (brace_token, _) = Brace::parse_with(input, |input| {
+                Punctuated::<_, Token![,]>::visit_parse_with(input, |input| {
                     input.parse::<Token![@]>()?;
                     let lookhead = input.lookahead1();
                     if lookhead.peek(kw::source) {
@@ -320,8 +374,8 @@ impl Parse for VariantBody {
             })
         } else if input.peek(token::Paren) {
             let mut anon_struct = None;
-            let (paren_token, _) = Parened::parse_with(input, |input| {
-                punctuated_parse::<Token![,], _>(input, |input| {
+            let (paren_token, _) = Paren::parse_with(input, |input| {
+                Punctuated::<_, Token![,]>::visit_parse_with(input, |input| {
                     anon_struct = Some(input.parse()?);
                     Ok(())
                 })
@@ -381,7 +435,7 @@ impl Parse for ContextField {
 }
 
 struct AnonStruct {
-    attr: Attributes,
+    attrs: Attributes,
     struct_token: Token![struct],
     body: StructBody,
 }
@@ -389,29 +443,39 @@ struct AnonStruct {
 impl AnonStruct {
     fn get_struct_def(&self, vis: &Visibility, name: &Ident) -> TokenStream {
         let Self {
-            attr,
+            attrs,
             struct_token,
             body,
         } = self;
-        let body = match body {
-            StructBody::Struct(named) => quote!(#named),
-            StructBody::Tuple(unnamed) => {
-                let content = quote!(#unnamed);
-                quote!(#content;)
-            }
-            StructBody::Unit => quote!(;),
+        let generic = body.get_generic_with(StructBody::get_generic_bounded_f);
+        let struct_body = body.get_struct_body(
+            |GenericField {
+                 generic,
+                 attrss,
+                 vis,
+                 ident,
+                 colon_token,
+                 ..
+             }| quote!(#attrss #vis #ident #colon_token #generic),
+        );
+        let semi = match struct_body.as_ref() {
+            Some(WithSurround { surround, .. }) => match surround {
+                SurroundEnum::Brace(..) => None,
+                SurroundEnum::Paren(..) => Some(quote!(;)),
+            },
+            None => Some(quote!(;)),
         };
-        quote!(#attr #vis #struct_token #name #body)
+        quote!(#attrs #vis #struct_token #name #generic #struct_body #semi)
     }
 }
 
 impl Parse for AnonStruct {
     fn parse(input: ParseStream) -> Result<Self> {
-        let attr = input.parse()?;
+        let attrs = input.parse()?;
         let struct_token = input.parse()?;
         let body = input.parse()?;
         Ok(Self {
-            attr,
+            attrs,
             struct_token,
             body,
         })
@@ -419,19 +483,146 @@ impl Parse for AnonStruct {
 }
 
 enum StructBody {
-    Struct(FieldsNamed),
-    Tuple(FieldsUnnamed),
+    Struct {
+        brace_token: Brace,
+        named: GenericFields,
+    },
+    Tuple {
+        paren_token: Paren,
+        unnamed: GenericFields,
+    },
     Unit,
+}
+
+impl StructBody {
+    fn get_struct_body<F>(
+        &self,
+        f: F,
+    ) -> Option<WithSurround<Punctuated<TokenStream, Token![,]>, SurroundEnum>>
+    where
+        F: FnMut(&GenericField) -> TokenStream,
+    {
+        match self {
+            Self::Struct { brace_token, named } => Some(WithSurround {
+                surround: SurroundEnum::Brace(brace_token.clone()),
+                content: named.0.iter().map(f).collect(),
+            }),
+            Self::Tuple {
+                paren_token,
+                unnamed,
+            } => Some(WithSurround {
+                surround: SurroundEnum::Paren(paren_token.clone()),
+                content: unnamed.0.iter().map(f).collect(),
+            }),
+            Self::Unit => None,
+        }
+    }
+
+    fn get_convert_struct_body_f(from: &TokenStream, field: &GenericField) -> TokenStream {
+        let GenericField {
+            ident, colon_token, ..
+        } = field;
+        let from_field = match ident {
+            FieldIdent::Some(ident) => quote!(#ident),
+            FieldIdent::None(idx) => TokenStream::from_str(&idx.to_string()).unwrap(),
+        };
+        quote!(#ident #colon_token #from.#from_field.into())
+    }
+
+    fn get_generic_with<F>(&self, f: F) -> Option<TokenStream>
+    where
+        F: FnMut(&GenericField) -> TokenStream,
+    {
+        self.get_struct_body(f).map(|WithSurround { content, .. }| {
+            WithSurround {
+                surround: AngleBracket(parse_quote!(<), parse_quote!(>)),
+                content,
+            }
+            .to_token_stream()
+        })
+    }
+
+    fn get_generic_bounded_f(field: &GenericField) -> TokenStream {
+        let GenericField { generic, ty, .. } = field;
+        quote!(#generic: Into<#ty>)
+    }
+
+    fn get_generic_name_f(field: &GenericField) -> TokenStream {
+        let GenericField { generic, .. } = field;
+        quote!(#generic)
+    }
+
+    fn get_generic_ty_f(field: &GenericField) -> TokenStream {
+        let GenericField { ty, .. } = field;
+        quote!(#ty)
+    }
 }
 
 impl Parse for StructBody {
     fn parse(input: ParseStream) -> Result<Self> {
         if input.peek(token::Brace) {
-            Ok(Self::Struct(input.parse()?))
+            let (brace_token, named) =
+                Brace::parse_with(input, |input| GenericFields::parse(true, input))?;
+            Ok(Self::Struct { brace_token, named })
         } else if input.peek(token::Paren) {
-            Ok(Self::Tuple(input.parse()?))
+            let (paren_token, unnamed) =
+                Paren::parse_with(input, |input| GenericFields::parse(false, input))?;
+            Ok(Self::Tuple {
+                paren_token,
+                unnamed,
+            })
         } else {
             Ok(Self::Unit)
+        }
+    }
+}
+
+struct GenericFields(Punctuated<GenericField, Token![,]>);
+
+impl GenericFields {
+    fn parse(named: bool, input: ParseStream) -> Result<Self> {
+        let mut idx = 0;
+        Ok(Self(Punctuated::parse_with(input, |input| {
+            let generic = custom_token(&format!("T{}", idx));
+            let attrss = input.parse()?;
+            let vis = input.parse()?;
+            let (ident, colon_token) = match named {
+                true => (FieldIdent::Some(input.parse()?), Some(input.parse()?)),
+                false => (FieldIdent::None(idx), None),
+            };
+            let ty = input.parse()?;
+            idx += 1;
+            Ok(GenericField {
+                generic,
+                attrss,
+                vis,
+                ident,
+                colon_token,
+                ty,
+            })
+        })?))
+    }
+}
+
+struct GenericField {
+    generic: Type,
+    attrss: Attributes,
+    vis: Visibility,
+    ident: FieldIdent,
+    colon_token: Option<Token![:]>,
+    ty: Type,
+}
+
+enum FieldIdent {
+    Some(Ident),
+    None(usize),
+}
+
+impl ToTokens for FieldIdent {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        match self {
+            Self::Some(ident) => ident.to_tokens(tokens),
+            Self::None(_) => (),
         }
     }
 }
--- impl/src/utils.rs
+++ impl/src/utils.rs
@@ -1,16 +1,16 @@
-use proc_macro2::TokenStream;
+use std::cell::RefCell;
+use std::iter::FromIterator;
+
+use proc_macro2::{Span, TokenStream};
 use quote::{quote, ToTokens};
 use syn::parse::{Parse, ParseStream, Result};
-use syn::{braced, parenthesized, token, Attribute};
+use syn::{braced, parenthesized, parse_quote, token, Attribute, Ident, Token};
 
-pub struct TokensWith<F>(F);
+pub struct TokensWith<'a>(RefCell<Box<dyn 'a + FnMut(&mut TokenStream)>>);
 
-impl<F> ToTokens for TokensWith<F>
-where
-    F: Fn(&mut TokenStream),
-{
+impl<'a> ToTokens for TokensWith<'a> {
     fn to_tokens(&self, tokens: &mut TokenStream) {
-        self.0(tokens)
+        self.0.borrow_mut()(tokens)
     }
 }
 
@@ -30,68 +30,238 @@ impl ToTokens for Attributes {
     }
 }
 
-pub struct Braced;
+pub struct WithSurround<T, S> {
+    pub surround: S,
+    pub content: T,
+}
 
-impl Braced {
-    pub fn parse_with<T, F>(input: ParseStream, f: F) -> Result<(token::Brace, T)>
+impl<T, S: ParseWith> WithSurround<T, S> {
+    pub fn parse_with<F>(input: ParseStream, f: F) -> Result<Self>
     where
         F: FnOnce(ParseStream) -> Result<T>,
     {
-        let braced;
-        Ok((braced!(braced in input), f(&braced)?))
+        let (surround, content) = S::parse_with(input, f)?;
+        Ok(Self { surround, content })
     }
 }
 
-pub struct Parened;
+impl<T, S: Surround> WithSurround<T, S> {
+    pub fn to_tokens_with<F>(&self, tokens: &mut TokenStream, f: F)
+    where
+        F: FnOnce(&mut TokenStream),
+    {
+        self.surround.surround(tokens, f)
+    }
+}
 
-impl Parened {
-    pub fn parse_with<T, F>(input: ParseStream, f: F) -> Result<(token::Paren, T)>
+impl<T: Parse, S: ParseWith> Parse for WithSurround<T, S> {
+    fn parse(input: ParseStream) -> Result<Self> {
+        Self::parse_with(input, T::parse)
+    }
+}
+
+impl<T: ToTokens, S: Surround> ToTokens for WithSurround<T, S> {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        self.to_tokens_with(tokens, |tokens| self.content.to_tokens(tokens))
+    }
+}
+
+pub trait ParseWith: Sized {
+    fn parse_with<T, F>(input: ParseStream, f: F) -> Result<(Self, T)>
+    where
+        F: FnOnce(ParseStream) -> Result<T>;
+}
+
+pub trait Surround {
+    fn surround<F>(&self, tokens: &mut TokenStream, f: F)
+    where
+        F: FnOnce(&mut TokenStream);
+}
+
+pub enum SurroundEnum {
+    Brace(Brace),
+    Paren(Paren),
+}
+
+impl Surround for SurroundEnum {
+    fn surround<F>(&self, tokens: &mut TokenStream, f: F)
+    where
+        F: FnOnce(&mut TokenStream),
+    {
+        match self {
+            Self::Brace(brace) => brace.surround(tokens, f),
+            Self::Paren(paren) => paren.surround(tokens, f),
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct Brace(pub token::Brace);
+
+impl ParseWith for Brace {
+    fn parse_with<T, F>(input: ParseStream, f: F) -> Result<(Self, T)>
     where
         F: FnOnce(ParseStream) -> Result<T>,
     {
-        let braced;
-        Ok((parenthesized!(braced in input), f(&braced)?))
+        let content;
+        let brace = braced!(content in input);
+        f(&content).map(|t| (Self(brace), t))
     }
 }
 
-pub fn tokens_with<F>(f: F) -> TokensWith<F>
-where
-    F: Fn(&mut TokenStream),
-{
-    TokensWith(f)
-}
-
-pub fn punctuated_parse<P, F>(input: ParseStream, mut f: F) -> Result<()>
-where
-    P: Parse,
-    F: FnMut(ParseStream) -> Result<()>,
-{
-    loop {
-        if input.is_empty() {
-            break;
-        }
-        f(input)?;
-        if input.is_empty() {
-            break;
-        }
-        input.parse::<P>()?;
+impl Surround for Brace {
+    fn surround<F>(&self, tokens: &mut TokenStream, f: F)
+    where
+        F: FnOnce(&mut TokenStream),
+    {
+        self.0.surround(tokens, f)
     }
-    Ok(())
 }
 
-pub fn punctuated_tokens<P, T, I>(tokens: &mut TokenStream, iter: I)
+#[derive(Clone, Copy)]
+pub struct Paren(pub token::Paren);
+
+impl ParseWith for Paren {
+    fn parse_with<T, F>(input: ParseStream, f: F) -> Result<(Self, T)>
+    where
+        F: FnOnce(ParseStream) -> Result<T>,
+    {
+        let content;
+        let paren = parenthesized!(content in input);
+        f(&content).map(|t| (Self(paren), t))
+    }
+}
+
+impl Surround for Paren {
+    fn surround<F>(&self, tokens: &mut TokenStream, f: F)
+    where
+        F: FnOnce(&mut TokenStream),
+    {
+        self.0.surround(tokens, f)
+    }
+}
+
+pub struct AngleBracket(pub Token![<], pub Token![>]);
+
+impl ParseWith for AngleBracket {
+    fn parse_with<T, F>(input: ParseStream, f: F) -> Result<(Self, T)>
+    where
+        F: FnOnce(ParseStream) -> Result<T>,
+    {
+        let lt = input.parse::<Token![<]>()?;
+        let t = f(input)?;
+        let gt = input.parse::<Token![>]>()?;
+        Ok((Self(lt, gt), t))
+    }
+}
+
+impl Surround for AngleBracket {
+    fn surround<F>(&self, tokens: &mut TokenStream, f: F)
+    where
+        F: FnOnce(&mut TokenStream),
+    {
+        self.0.to_tokens(tokens);
+        f(tokens);
+        self.1.to_tokens(tokens);
+    }
+}
+
+pub fn tokens_with<'a, F>(f: F) -> TokensWith<'a>
 where
-    P: ToTokens + Default,
-    T: ToTokens,
-    I: IntoIterator<Item = T>,
+    F: 'a + FnMut(&mut TokenStream),
 {
-    let punct = P::default();
-    iter.into_iter().for_each(|item| {
-        let item = item.to_token_stream();
-        if item.is_empty() {
-            return;
+    TokensWith(RefCell::new(Box::new(f)))
+}
+
+pub struct Punctuated<T, P>(pub syn::punctuated::Punctuated<T, P>);
+
+impl<T, P> Punctuated<T, P> {
+    pub fn iter(&self) -> syn::punctuated::Iter<T> {
+        self.0.iter()
+    }
+}
+
+impl<P: Parse> Punctuated<(), P> {
+    pub fn visit_parse_with<F>(input: ParseStream, mut f: F) -> Result<()>
+    where
+        F: FnMut(ParseStream) -> Result<()>,
+    {
+        loop {
+            if input.is_empty() {
+                break;
+            }
+            f(input)?;
+            if input.is_empty() {
+                break;
+            }
+            input.parse::<P>()?;
         }
-        item.to_tokens(tokens);
-        punct.to_tokens(tokens);
-    })
+        Ok(())
+    }
+}
+
+impl<T, P: Parse> Punctuated<T, P> {
+    pub fn parse_with<F>(input: ParseStream, mut f: F) -> Result<Self>
+    where
+        F: FnMut(ParseStream) -> Result<T>,
+    {
+        let mut inner = syn::punctuated::Punctuated::new();
+        loop {
+            if input.is_empty() {
+                break;
+            }
+            inner.push_value(f(input)?);
+            if input.is_empty() {
+                break;
+            }
+            inner.push_punct(input.parse::<P>()?);
+        }
+        Ok(Self(inner))
+    }
+}
+
+impl<T: Parse, P: Parse> Parse for Punctuated<T, P> {
+    fn parse(input: ParseStream) -> Result<Self> {
+        Self::parse_with(input, T::parse)
+    }
+}
+
+impl<T, P: ToTokens> Punctuated<T, P> {
+    pub fn to_tokens_with<'a, F>(&'a self, f: F) -> TokensWith
+    where
+        F: 'a + FnMut(&T) -> TokenStream,
+    {
+        let mut f = f;
+        tokens_with(move |tokens| {
+            self.0.pairs().for_each(|p| {
+                f(p.value()).to_tokens(tokens);
+                p.punct().to_tokens(tokens)
+            })
+        })
+    }
+}
+
+impl<T: ToTokens, P: ToTokens> ToTokens for Punctuated<T, P> {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        self.to_tokens_with(T::to_token_stream).to_tokens(tokens)
+    }
+}
+
+impl<T, P: Default> FromIterator<T> for Punctuated<T, P> {
+    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+        let mut inner = syn::punctuated::Punctuated::new();
+        for item in iter {
+            inner.push(item)
+        }
+        Self(inner)
+    }
+}
+
+pub fn custom_ident(s: &str) -> Ident {
+    Ident::new(s, Span::call_site())
+}
+
+pub fn custom_token<T: Parse>(s: &str) -> T {
+    let ident = custom_ident(s);
+    parse_quote!(#ident)
 }
